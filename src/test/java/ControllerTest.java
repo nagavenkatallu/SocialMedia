0c083fcfc3aec0377709ca240c9fd68dda6cf544
nagavenkatallu <venkatallu97@gmail.com>
2021-10-19 15:21:47 +0530

    addes ControllerTest

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerTest {
+
+	@Test
+	public void testLogin() {
+		User us=new User();
+		us.setUsername("venky123");
+		us.setPassword("venky123");
+		assertEquals(true,new Controller().login(us));;
+	}
+
+	@Test
+	public void testRegisterUser() {
+		User us=new User();
+		us.setFirstname("dummy");
+		us.setLastname("dummy");
+		us.setUsername("dummy123");
+		us.setPassword("dummy123");
+		us.setEmail("dummy@gmail.com");
+		us.setGender("male");
+		us.setAge(18);
+		assertEquals(true,new Controller().registerUser(us));
+	}
+
+	@Test
+	public void testForgotPassword() {
+		User us=new User();
+		us.setUsername("venky123");
+		us.setPassword("venky123");
+		us.setAge(21);
+		assertEquals(true,new Controller().forgotPassword(us));
+	}
+
+}
